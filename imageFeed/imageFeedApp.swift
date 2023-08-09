66413249277fb8e2c96e6825a3aaca60cbc49617
helloEvory <elijahegirev@gmail.com>
2023-08-09 15:24:47 +0300

    Initial Commit

---
+++ imageFeed/imageFeedApp.swift
@@ -0,0 +1,17 @@
+//
+//  imageFeedApp.swift
+//  imageFeed
+//
+//  Created by Илюха Кодинг on 09.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct imageFeedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
